7378d9ea996f45267ae4dc71826a6318a1a542ba
Eir Wilson <tech+git@virunus.com>
2023-04-17 20:38:26 -0400

    adjust logging

--- functions/secure/_middleware.ts
+++ functions/secure/_middleware.ts
@@ -7,7 +7,7 @@ interface Env {
 }
 
 export const onRequest: PagesFunction<Env> = (context) => {
-    console.log('secure: checking auth')
+    console.log('secure: checking auth with CF_PAGES=' + context.env.CF_PAGES)
     if (context.env.CF_PAGES === 1) {
         console.log('secure: verifiying zero trust')
         return cloudflareAccessPlugin({
